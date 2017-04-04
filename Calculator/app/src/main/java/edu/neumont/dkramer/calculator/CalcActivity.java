8c58285869941ab9d8d9f4fb6cc208a0e6ddbc92
David Kramer <dkramer@student.neumont.edu>
2017-04-04 13:41:27 -0600

    Initial commit of calculator project

---
+++ Calculator/app/src/main/java/edu/neumont/dkramer/calculator/CalcActivity.java
@@ -0,0 +1,13 @@
+package edu.neumont.dkramer.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
