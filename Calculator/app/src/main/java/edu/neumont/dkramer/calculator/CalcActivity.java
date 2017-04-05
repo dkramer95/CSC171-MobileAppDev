245c892a1378a47e505fc25275b1be6aa1585ac6
David Kramer <dkramer@student.neumont.edu>
2017-04-04 18:14:39 -0600

    Added basic calcButtonClicked handler

--- Calculator/app/src/main/java/edu/neumont/dkramer/calculator/CalcActivity.java
+++ Calculator/app/src/main/java/edu/neumont/dkramer/calculator/CalcActivity.java
@@ -2,6 +2,9 @@ package edu.neumont.dkramer.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class CalcActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class CalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+    }
+
+    public void calcButtonClicked(View view)
+    {
+        Button btn = (Button)view;
+        String text = btn.getText().toString();
+        Log.i("Info", "Button: " + text + ", was pressed!");
     }
 }
