d6379342cb05c8c376a8a101b34ae949cb375ac0
David Kramer <dkramer@student.neumont.edu>
2017-04-09 10:53:13 -0600

    Added save to CSV file

--- Calculator/app/src/main/java/edu/neumont/dkramer/calculator/CalcActivity.java
+++ Calculator/app/src/main/java/edu/neumont/dkramer/calculator/CalcActivity.java
@@ -8,6 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CalcActivity extends AppCompatActivity {
     protected static CalculatorModel calcModel;
@@ -39,6 +45,39 @@ public class CalcActivity extends AppCompatActivity {
         calcView.setText(calcModel.getCalcText());
     }
 
+    public void saveResultToCSV(View view) {
+        String path = getFilesDir().getPath().toString() + "/calcResults.csv";
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
+        try {
+            File file = new File(path);
+	        String data = calcModel.getRunningTotalDisplay() + ",";
+
+	        if (!file.exists()) {
+		        file.createNewFile();
+	        }
+	        fw = new FileWriter(file.getAbsoluteFile(), true);
+	        bw = new BufferedWriter(fw);
+	        bw.write(data);
+
+	        Toast.makeText(this, "Saved result to CSV file", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+	        Toast.makeText(this, "Failed to save result to file!", Toast.LENGTH_SHORT).show();
+        } finally {
+	        try {
+		        if (bw != null) {
+			        bw.close();
+		        }
+		        if (fw != null) {
+			        fw.close();
+		        }
+	        } catch (IOException e) {
+		        Log.e("ERROR", "Failed to close stream(s)");
+	        }
+        }
+    }
+
     public void binaryModeButtonClicked(View view) {
         Intent binaryCalcActivity = new Intent(this, BinaryCalcActivity.class);
         startActivity(binaryCalcActivity);
@@ -56,7 +95,6 @@ public class CalcActivity extends AppCompatActivity {
 
     public void equalsCalcButtonClicked(View view) {
         TextView numView = (TextView)(findViewById(R.id.calcRunningTotalView));
-
         numView.setText(calcModel.getRunningTotalDisplay());
     }
 
